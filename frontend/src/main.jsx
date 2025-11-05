48d8e3b27c4eac10bef479125a64808dd721d40a
Shrey Mehrotra <shrey12mehrotra@gmail.com>
2025-11-05 20:14:14 +0530

    updated UI/UX of the app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import theme from './theme.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+     <ThemeProvider theme={theme}>
+      <CssBaseline />
     <App />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
+
+
+
